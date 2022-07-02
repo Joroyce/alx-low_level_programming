d3b960dc9017c0f3b49a5f4e2afd69e154f7cd7f
Joroyce <joroyce2412@gmail.com>
2022-07-01 17:01:53 -0700

    All for now

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,6 @@ int main(void)
 {
 	int n, lastd;
 
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
@@ -28,5 +27,6 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
+
 	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,25 @@
-
 #include <stdio.h>
 
 /**
  * main - Prints the alphabetic in lower and upper case
  *
  * Return: Always (Success)
- * /
- int main(void)
- {
- char c;
+ */
+int main(void)
+{
+	char c;
 
- for (c = 'a'; c <= 'z'; c++)
- {
- putchar(c);
- }
- for (c = 'A'; c <= 'Z'; c++)
- {
- putchar(c);
- }
- putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
 
- return (0);
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
